378094ec196cbe6944b2f889735d5b4ae7c51c39
ahmd-tauqr <ahmd.tauqr@gmail.com>
2024-05-28 19:56:32 +0530

    fixed header nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={`${inter.className} pt-16 md:pt-20`}>
+      <body className={`${inter.className}`}>
         <AuthProvider>
           <Toaster />
           <Nav />
